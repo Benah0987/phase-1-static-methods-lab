e85e08f35ff5b04eeccb25f22c2f9dd1dcbd6861
Benah0987 <wanyoikebenayah77@gmail.com>
2023-04-16 12:34:23 +0300

    added static function

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    const caps = string.charAt(0).toUpperCase() + string.slice(1);
+    return caps;
+
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  static titleize(string) {
+    const nonCapitalizedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = string.split(' ');
+
+    const capitalizedWords = words.map((word, index) => {
+      if (index === 0 || !nonCapitalizedWords.includes(word)) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word;
+      }
+    });
+
+    return capitalizedWords.join(' ');
+  }
 }
+//console log capitalize
+const capitalizedString = Formatter.capitalize('hello world');
+console.log(capitalizedString); 
+
+//console log sanitize
+const sanitizedString = Formatter.sanitize('hello!@#world');
+console.log(sanitizedString); 
+//console log titlesize
+const titleizedString = Formatter.titleize('the quick brown fox jumps over the lazy dog');
+console.log(titleizedString); 
+
